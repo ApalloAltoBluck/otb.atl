5d2931b8f03b165633fc3c6bf851e0c7f5a8b8f0
Oliver buckley <bluckstar1@gmail.com>
2022-01-01 17:48:24 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Outside the Box",
+  },
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-react-helmet"],
+};
