13bb0e38f36f176037db9c5dfdaec0951573ad09
Oliver buckley <bluckstar1@gmail.com>
2022-01-02 15:48:00 -0500

    fixed button sizing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,137 +1,12 @@
 import * as React from "react"
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
-
 // turn to switch statement
 const HomepageContent = (props) => {
-  if (props.slide==1) {
-      return (
-        <h1>Slide one</h1>
-      )
-  } else if (props.slide==2) {
+  if (props.slide == 1) {
+    return (
+      <h1>Slide one</h1>
+    )
+  } else if (props.slide == 2) {
     return (
       <h1>SLIDE TWO</h1>
     )
@@ -140,29 +15,33 @@ const HomepageContent = (props) => {
 }
 
 const NavigateContent = (props) => {
-  return (
-    <div className="align-center text-center mt-7">
+  return (<>
+    {!props.headerShow &&
+    <div className="align-center text-center bt-7 mt-7">
       <p onClick={() => props.setSlide(1)} className="align-center font-bold fill-stone-600">ROADMAP</p>
       <p onClick={() => props.setSlide(2)} className="align-center font-bold fill-stone-600">CATALOGUE</p>
       <p className="align-center font-bold fill-stone-600">CONTACT US</p>
-    </div>
+    </div>} </>
   )
 }
 const IndexContent = (props) => {
-  return(
+  return (
 
-<>
- {props.headerShow && 
-  <div className="mx-auto mt-10"  >
+    <>
+      {props.headerShow &&
+        <div className="mx-auto mt-10 mb-10"  >
 
-  <p className="text-black m-3 uppercase text-xs md:text-md text-large text-center "><b>sign up to receive information and updates on upcoming releases, products, and other news.</b></p>
-  <div className="mx-auto flex justify-center">
-  <input type="text" id="fname" placeholder="YOUR EMAIL" className="  rounded-lg origin-center w-1/2 bg-gray-800 text-white" ></input> 
-  <div className="break-before-page  rounded-full pb-1 outline outline-2 font-bold text-xs pl-5 pr-5 pt-1" onClick={() => props.setHeaderShow(false) }>SIGN UP</div>
-  </div>
-</div>
-}
-</>
+          <p className="text-black m-3 uppercase text-xs md:text-md text-large text-center w-3/4 mx-auto "><b>sign up to receive information and updates on upcoming releases, products, and other news.</b></p>
+          <div className="mx-auto flex justify-center">
+            <div className=" w-3/4">
+              <input type="text" id="fname" placeholder="YOUR EMAIL" className="h-3/4 shadow border rounded w-full py-2 px-3 text-gray-700 appearance-none leading-tight focus:outline-none focus:shadow-outline w-50" ></input>
+            </div>
+          </div>
+          <div className="w-1/2 mx-auto l:w-1/5 rounded-full whitespace-nowrap pb-1 outline outline-2 font-bold text-xs my-auto pl-4 pr-4 pt-1 text-center mt-3 " onClick={() => props.setHeaderShow(false)}>SIGN UP</div>
+
+        </div>
+      }
+    </>
   )
 }
 // markup
@@ -174,21 +53,21 @@ const IndexPage = () => {
     <main className="container mx-auto">
       <title>Home Page</title>
       <div className="grid grid-cols-1 w-1/2 md:w-1/3 mt-28 md:mt-60 my-auto rounded-3xl mx-auto shadow-lg">
-      <img className="mt-10 w-1/2 mx-auto" src="otb.png" />
+        <img className="mt-10 w-1/2 mx-auto" src="otb.png" />
 
-    <IndexContent headerShow ={headerShow} setHeaderShow={setHeaderShow}></IndexContent>
-    <HomepageContent slide={slide}></HomepageContent>
-    <NavigateContent slide={slide} setSlide={setSlide}></NavigateContent>
+        <IndexContent headerShow={headerShow} setHeaderShow={setHeaderShow}></IndexContent>
+        <HomepageContent slide={slide}></HomepageContent>
+        <NavigateContent headerShow={headerShow} slide={slide} setSlide={setSlide}></NavigateContent>
 
-    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-width="24" height="24"
-viewBox="0 0 24 24" className="mx-auto m-10"
->    
-<path d="M 8 3 C 5.243 3 3 5.243 3 8 L 3 16 C 3 18.757 5.243 21 8 21 L 16 21 C 18.757 21 21 18.757 21 16 L 21 8 C 21 5.243 18.757 3 16 3 L 8 3 z M 8 5 L 16 5 C 17.654 5 19 6.346 19 8 L 19 16 C 19 17.654 17.654 19 16 19 L 8 19 C 6.346 19 5 17.654 5 16 L 5 8 C 5 6.346 6.346 5 8 5 z M 17 6 A 1 1 0 0 0 16 7 A 1 1 0 0 0 17 8 A 1 1 0 0 0 18 7 A 1 1 0 0 0 17 6 z M 12 7 C 9.243 7 7 9.243 7 12 C 7 14.757 9.243 17 12 17 C 14.757 17 17 14.757 17 12 C 17 9.243 14.757 7 12 7 z M 12 9 C 13.654 9 15 10.346 15 12 C 15 13.654 13.654 15 12 15 C 10.346 15 9 13.654 9 12 C 9 10.346 10.346 9 12 9 z"></path></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+          width="24" height="24"
+          viewBox="0 0 24 24" className="mx-auto m-10"
+        >
+          <path d="M 8 3 C 5.243 3 3 5.243 3 8 L 3 16 C 3 18.757 5.243 21 8 21 L 16 21 C 18.757 21 21 18.757 21 16 L 21 8 C 21 5.243 18.757 3 16 3 L 8 3 z M 8 5 L 16 5 C 17.654 5 19 6.346 19 8 L 19 16 C 19 17.654 17.654 19 16 19 L 8 19 C 6.346 19 5 17.654 5 16 L 5 8 C 5 6.346 6.346 5 8 5 z M 17 6 A 1 1 0 0 0 16 7 A 1 1 0 0 0 17 8 A 1 1 0 0 0 18 7 A 1 1 0 0 0 17 6 z M 12 7 C 9.243 7 7 9.243 7 12 C 7 14.757 9.243 17 12 17 C 14.757 17 17 14.757 17 12 C 17 9.243 14.757 7 12 7 z M 12 9 C 13.654 9 15 10.346 15 12 C 15 13.654 13.654 15 12 15 C 10.346 15 9 13.654 9 12 C 9 10.346 10.346 9 12 9 z"></path></svg>
       </div>
 
-      
-  
+
+
     </main>
   )
 }
